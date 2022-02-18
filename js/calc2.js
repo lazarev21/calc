eb89ebd0f7dc2dbfd6cf999dcc4c080cc0e306b2
lazarev21 <lazarevg@bk.ru>
2022-02-18 11:35:46 +0300

    //

---
+++ js/calc2.js
@@ -0,0 +1,150 @@
+const container = document.querySelector('.container')
+const result = document.querySelector('.resultLog')
+const actions = document.querySelector('.actionsLog')
+
+let arrayOfClicks = [] //массив для записи истории кликов и дальнеших рассчетов
+let strOfClicksOnStep = '' //строка для записи кликов на текущем шаге
+
+container.addEventListener('click', function (event)  {
+    let target = event.target;
+
+    if (target.closest('.btn-value')) { //
+        if (strOfClicksOnStep.length < 6) { // ограничиваем количество символов для ввода
+        strOfClicksOnStep += target.innerHTML; //прибавляем значение от пользователя в строку 
+        arrayOfClicks.push(target.innerHTML); //прибавляем значения в наш массив
+        result.innerHTML = strOfClicksOnStep; //показываем текущие значения пользователю
+        }
+    }
+
+    if (target.closest('.btn-action')) {
+        arrayOfClicks.push(target.innerHTML) //добавляем дейсвтие в наш массив
+        actions.innerHTML = arrayOfClicks.join('') //выводим историю дейсвтий
+        strOfClicksOnStep = ''; //обнуялем строку на текущем шаге
+    }
+
+    if (target.closest('.btn-backspace')) {
+        let strOfResult = result.innerHTML
+        //проверяем длину строки, чтобы не стирать ее до конца, а заменить на 0
+        if (strOfResult.length === 1) { 
+            result.innerHTML = 0;
+            strOfClicksOnStep = '';
+            arrayOfClicks.splice(-1,1);
+        }
+        
+        if (strOfResult.length > 1) {
+            strOfClicksOnStep = strOfResult.slice(0,-1);
+            result.innerHTML = strOfClicksOnStep
+            arrayOfClicks.splice(-1,1);
+        }
+    }
+
+    if (target.closest('.btn-clear')) { 
+        arrayOfClicks = []; //обнуляем массив
+        strOfClicksOnStep = ''; //обнуляем строку
+        actions.innerHTML = '0'; //обнуляем историю действий
+        result.innerHTML = '0'; //обнуляем результат
+        result.style['font-size'] = "96px" //возвращем размер по умолчанию
+    }
+
+    if (target.closest('.btn-result')) {
+        console.log(arrayOfClicks)
+           /*  if (result.innerHTML.includes('+') || result.innerHTML.includes('–')
+             || result.innerHTML.includes('÷') || result.innerHTML.includes('×')) { */
+            result.innerHTML = checkArgumentsForCalc(arrayOfClicks);
+            arrayOfClicks = [];
+            arrayOfClicks.push(result.innerHTML);
+           /*   }
+            else {
+            arrayOfClicks.push('+')
+            result.innerHTML = checkArgumentsForCalc(arrayOfClicks);
+            arrayOfClicks = [];
+            arrayOfClicks.push(result.innerHTML);
+            } */
+        console.log(arrayOfClicks)
+  
+
+      /*   if (strResultLog.length > 6) { //меняем размер шрифта, если результат слишком длинный
+            result.style['font-size'] = "48px"
+            result.innerHTML = strResultLog;
+        }   
+        else {
+            result.innerHTML = strResultLog;
+        } */
+    }
+
+})  
+
+function checkArgumentsForCalc(arrayOfClicks) {
+    const operationsFromUi = { //объект для поиска дейсвтия, на которое кликнул пользователь
+        '+': 'sum',
+        '–': 'sub',
+        '÷': 'div',
+        '×': 'mult',
+    }
+
+    for (let i = 0; i <arrayOfClicks.length; i++) { //перебираем каждый элемент строки, пока не найдем действие
+        if (arrayOfClicks[i] in operationsFromUi) { 
+            // когжа нашли дейсвтие делим записываем значения a,b ДО и После дейсвтия соотвественно
+            a = +(arrayOfClicks.slice([0], [i]).join(''));
+            b = +(arrayOfClicks.slice([i+1], [arrayOfClicks.length-1]).join(''))
+            operation = operationsFromUi[arrayOfClicks[i]]; //оператор для фукнции Calc
+
+            if (b == 0) {
+                b = a;
+            }
+            if (operation == 0) {
+                b = 0
+                operation = "+"
+            }
+            if ((a != '') & (b != '') & (operation != '')) {
+                return(Calc(operation, a, b))
+            }
+
+            }
+        
+        }
+    
+    
+    
+}
+
+function Calc (operation, a, b) {
+    const isNotValid = isNaN(a) || isNaN(b);
+    const operations = {
+        sum: a+b,
+        sub: a-b,
+        div: a/b,
+        mult: a*b,
+    }
+    if (isNotValid) {
+        return ('error');
+    }
+    else {
+        if (operation in operations) {
+            return( operations[operation])
+        }
+        else {
+            return ('Unknown operation')
+        }
+
+}
+}
+
+
+
+
+    /* if (event.target.closest('.btn-value') || event.target.closest('.btn-action')) {
+        
+        arrayOfClicks+=target.innerHTML; //добавялем в строку значения, который накликал пользователь
+        actions.innerHTML = arrayOfClicks;  //выводим в историю дейсвтий
+        
+        function clickUI () {
+            event.target.style['opacity'] = "0.7";
+        }
+        function clickUIBack () {
+            event.target.style['opacity'] = "1";
+        }
+        
+        setTimeout (clickUI);
+        setTimeout (clickUIBack,100);
+    } */
